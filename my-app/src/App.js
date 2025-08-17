57b88ee73dc9b8e9aa9cda6ecbb2ee9784cd2d07
eyuil <43527367+eyu-il@users.noreply.github.com>
2025-08-16 17:41:49 -0700

    Created Front Page
    added MAL sign in button
    added title
    added arrow to direct users to scroll down
    no functionality yet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+function MALSignIn() {
+  return(
+    <button className="MAL-sign-in-button">Sign in with MyAnimeList</button>
+  );
+}
+
+function ArrowToNextPage() {
+  return(
+    <img src="/downarrow.png" className="Arrow" alt="Arrow to next page"></img>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tokku</h1>
+        <MALSignIn/>
+        <ArrowToNextPage/>
       </header>
     </div>
   );
 }
+
 
 export default App;
